f9c1610254d32c83d450fa082ed83f1a192c99fe
Milko Todorov <milko.todorov@sap.com>
2023-08-27 21:18:55 +0200

    Optimization & Bugfix:  - Reduce the charging power to 0.15C for the last 5% in order to achieve higher stop charging accuracy  - Bugfix: Wrong "Current backup reserve displayed in the logs"  - Minor refactoring

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ SCRIPTDIR=$(cd $(dirname $0) > /dev/null 2>&1 && pwd)
 
 pushd $SCRIPTDIR > /dev/null
 source venv/bin/activate
-python3 se_battery_control.py x.x.x.x
+python3 se_battery_control.py inverter_ip
 popd > /dev/null
--- se_battery_control.py
+++ se_battery_control.py
@@ -15,11 +15,11 @@ CONFIG = []
 
 # Configuration parameters to be applied to the inverter with initial/default values. 
 # Actual values will be read from 'config.yaml'
-UPDATE_INTERVAL = 120        # Update interval if used as service / from the console.
-UPPER_CHARGING_LIMIT = 80    # Upper charging limit in %
-SOE_DELTA_CHARGE = 5         # When the SOE drops by this amount of %, start charging again
-BACKUP_RESEVE = 10           # Charge in % reserved only for backup + SE Home Batteries 48V has 10% reserved energy which cannot be changed/used
-CHARGE_LIMIT = 5000          # Battery maximum charge power in W
+UPDATE_INTERVAL = 120      # Update interval if used as service / from the console.
+UPPER_CHARGING_LIMIT = 80  # Upper charging limit in %
+SOE_DELTA_CHARGE = 5       # When the SOE drops by this amount of %, start charging again
+BACKUP_RESEVE = 10         # Charge in % reserved only for backup + SE Home Batteries 48V has 10% reserved energy which cannot be changed/used
+CHARGE_LIMIT = 5000        # Battery maximum charge power in W
 
 #
 # Reading the configuration parameters from config.yaml file.
@@ -39,7 +39,7 @@ def read_config(default=False):
   with open('config.yaml', 'r') as file:
     CONFIG = yaml.safe_load(file)
 
-  if (default): 
+  if default:
     UPDATE_INTERVAL = CONFIG["defaul_config"]["update_interval"]
     UPPER_CHARGING_LIMIT = CONFIG["defaul_config"]["upper_charging_limit"]
     SOE_DELTA_CHARGE = CONFIG["defaul_config"]["soe_delta_charge"]
@@ -47,7 +47,7 @@ def read_config(default=False):
     CHARGE_LIMIT = CONFIG["defaul_config"]["charge_limit"]
     log_config()
     return
-  
+
   periods = CONFIG["periods"]
 
   for period in periods:
@@ -61,12 +61,13 @@ def read_config(default=False):
       f"{period_end[1]} {period_end[0]}, {today_datetime.year} 23:59:59", "%b %d, %Y %H:%M:%S"
     )
 
-    if (period_start_datetime <= today_datetime  <= period_end_datetime):
+    if period_start_datetime <= today_datetime <= period_end_datetime:
       UPPER_CHARGING_LIMIT = period["config"]["upper_charging_limit"]
       SOE_DELTA_CHARGE = period["config"]["soe_delta_charge"]
       BACKUP_RESEVE = period["config"]["backup_reserve"]
       CHARGE_LIMIT = period["config"]["charge_limit"]
       log_config()
+      
 #
 # Log the configuration parameters
 #
@@ -81,7 +82,6 @@ def log_config():
 # Read all values from the inverter
 #
 def read_values():
-  values = {}
   values = inverter.read_all()
   meters = inverter.meters()
   batteries = inverter.batteries()
@@ -113,17 +113,17 @@ def set_storage_control_mode(val=4, retries=3):
   # GitHub issue: https://github.com/nmakel/solaredge_modbus/issues/36
   try:
     retry_count = retries
-    while (retry_count > 0):
+    while retry_count > 0:
       if not inverter.connected():
         inverter.connect()
       retry_count = retry_count - 1
       reg_query = storage.write("storage_control_mode", val)
       reg_result = storage.read("storage_control_mode")
 
-      if (is_response_exception(reg_query)):
+      if is_response_exception(reg_query):
         LOGGER.error(f"Setting \"storage_control_mode\" (0xE004) to {val}. Error: " + str(reg_query.message))
         LOGGER.info(f"Retrying write to register...{retries - retry_count + 1} of {retries}")
-        if (retry_count == 0):
+        if retry_count == 0:
           raise Exception(str(reg_query.message))
         else:
           # Wait a bit before the next retry
@@ -145,29 +145,29 @@ def set_storage_backup_reserved(val=10, retries=3):
   # GitHub issue: https://github.com/nmakel/solaredge_modbus/issues
   try:
     retry_count = retries
-    while (retry_count > 0):
+    while retry_count > 0:
       if not inverter.connected():
         inverter.connect()
       retry_count = retry_count - 1
       reg_query = storage.write("storage_backup_reserved_setting", val)
       reg_result = storage.read("storage_backup_reserved_setting")
 
-      if (is_response_exception(reg_query)):
+      if is_response_exception(reg_query):
         LOGGER.error(f"Setting \"storage_backup_reserved_setting\" (0xE008) to {val}%. Error: " + str(reg_query.message))
         LOGGER.info(f"Retrying write to register...{retries - retry_count + 1} of {retries}")
-        if (retry_count == 0):
+        if retry_count == 0:
           raise Exception(str(reg_query.message))
         else:
           # Wait a bit before the next retry
           LOGGER.info("Waiting for 10 sec. before the next retry...")
-          time.sleep(10)        
+          time.sleep(10)
       else:
         verify_register_write("storage_backup_reserved_setting", val, reg_query, reg_result)
         break
   except Exception as err:
     LOGGER.error(f"Setting \"storage_backup_reserved_setting\" (0xE008) to {val}%.")
     LOGGER.exception(err, stack_info=True, exc_info=True)
-   
+
 #
 # Set "storage_default_mode" (0xE00A) - storage charge / discharge default mode
 #
@@ -184,22 +184,22 @@ def set_storage_default_mode(val=7, retries=3):
   # GitHub issue: https://github.com/nmakel/solaredge_modbus/issues
   try:
     retry_count = retries
-    while (retry_count > 0):
+    while retry_count > 0:
       if not inverter.connected():
         inverter.connect()
       retry_count = retry_count - 1
       reg_query = storage.write("storage_default_mode", val)
       reg_result = storage.read("storage_default_mode")
-    
-      if (is_response_exception(reg_query)):
+
+      if is_response_exception(reg_query):
         LOGGER.error(f"Setting \"storage_default_mode\" (0xE00A) to {val}. Error: " + str(reg_query.message))
         LOGGER.info(f"Retrying write to register...{retries - retry_count + 1} of {retries}")
-        if (retry_count == 0):
+        if retry_count == 0:
           raise Exception(str(reg_query.message))
         else:
           # Wait a bit before the next retry
           LOGGER.info("Waiting for 10 sec. before the next retry...")
-          time.sleep(10)        
+          time.sleep(10)
       else:
         verify_register_write("storage_default_mode", val, reg_query, reg_result)
         break
@@ -215,15 +215,15 @@ def set_rc_charge_limit(val=5000):
     reg_query = storage.write("rc_charge_limit", val)
     reg_result = storage.read("rc_charge_limit")
 
-    if (is_response_exception(reg_query)):
+    if is_response_exception(reg_query):
       LOGGER.error(f"Setting \"rc_charge_limit\" (0xE00E) to {val}Wh. Error: " + str(reg_query.message))
       return
-    
-    verify_register_write("rc_charge_limit", val, reg_query, reg_result)   
+
+    verify_register_write("rc_charge_limit", val, reg_query, reg_result)
   except Exception as err:
     LOGGER.error(f"Setting \"rc_charge_limit\" (0xE00E) to {val}Wh.")
     LOGGER.exception(err, stack_info=True, exc_info=True)
-    
+
 #
 # Set "rc_discharge_limit" (0xE010)
 #
@@ -232,11 +232,11 @@ def set_rc_discharge_limit(val=5000):
     reg_query = storage.write("rc_discharge_limit", val)
     reg_result = storage.read("rc_discharge_limit")
 
-    if (is_response_exception(reg_query)):
+    if is_response_exception(reg_query):
       LOGGER.error(f"Setting \"rc_discharge_limit\" (0xE010) to {val}Wh. Error: " + str(reg_query.message))
-      return  
-    
-    verify_register_write("rc_discharge_limit", val, reg_query, reg_result)   
+      return
+
+    verify_register_write("rc_discharge_limit", val, reg_query, reg_result)
   except Exception as err:
     LOGGER.error(f"Setting \"rc_discharge_limit\" (0xE010) to {val}Wh.")
     LOGGER.exception(err, stack_info=True, exc_info=True)
@@ -248,13 +248,13 @@ def set_rc_cmd_timeout(val=3600):
   try:
     reg_query = storage.write("rc_cmd_timeout", val)
     reg_result = storage.read("rc_cmd_timeout")
-    
-    if (is_response_exception(reg_query)):
+
+    if is_response_exception(reg_query):
       LOGGER.error(f"Setting \"rc_cmd_timeout\": {val} sec. Error: " + str(reg_query.message))
       return
-    
+
     verify_register_write("rc_cmd_timeout", val, reg_query, reg_result)
-  except Exception as err: 
+  except Exception as err:
     LOGGER.error(f"Setting \"rc_cmd_timeout\": {val} sec.")
     LOGGER.exception(err, stack_info=True, exc_info=True)
 
@@ -273,12 +273,12 @@ def set_rc_cmd_mode(val=0):
     reg_query = storage.write("rc_cmd_mode", val)
     reg_result = storage.read("rc_cmd_mode")
 
-    if (is_response_exception(reg_query)):
+    if is_response_exception(reg_query):
       LOGGER.error(f"Set \"rc_cmd_mode\" (0xE00A) to {val}. Error: " + str(reg_query.message))
       return
-    
+
     verify_register_write("rc_cmd_mode", val, reg_query, reg_result)
-  except Exception as err: 
+  except Exception as err:
     LOGGER.error(f"Set \"rc_cmd_mode\" (0xE00A) to {val}.")
     LOGGER.exception(err, stack_info=True, exc_info=True)
 
@@ -286,14 +286,14 @@ def set_rc_cmd_mode(val=0):
 # Check whether the response contains exception
 #
 def is_response_exception(reg_query):
-  if (type(reg_query) is (pymbEx.ModbusIOException or 
-                          pymbEx.ConnectionException or
-                          pymbEx.InvalidMessageReceivedException or
-                          pymbEx.ModbusException or
-                          pymbEx.NoSuchSlaveException or
-                          pymbEx.ParameterException or
-                          pymbEx.MessageRegisterException or
-                          pymbEx.NotImplementedException)):
+  if type(reg_query) is (pymbEx.ModbusIOException or
+                         pymbEx.ConnectionException or
+                         pymbEx.InvalidMessageReceivedException or
+                         pymbEx.ModbusException or
+                         pymbEx.NoSuchSlaveException or
+                         pymbEx.ParameterException or
+                         pymbEx.MessageRegisterException or
+                         pymbEx.NotImplementedException):
     return True
   else:
     return False
@@ -304,16 +304,16 @@ def is_response_exception(reg_query):
 #
 def verify_register_write(register_name, exp_val, reg_query, reg_result):
   func_code = reg_query.function_code
-  if (func_code >= 0x80):
+  if func_code >= 0x80:
     LOGGER.error(f"Error writing to register \"{register_name}\". " +
-                  f"Returned \"function_code\" is {func_code}. Should below 128 (0x80).")
+                 f"Returned \"function_code\" is {func_code}. Should below 128 (0x80).")
     return False
-  
+
   reg_val = reg_result[register_name]
-  if (reg_val != exp_val):
-    LOGGER.critical(f"Written register value for \"{register_name}\" is {reg_val} and should have been {exp_val}") 
+  if reg_val != exp_val:
+    LOGGER.critical(f"Written register value for \"{register_name}\" is {reg_val} and should have been {exp_val}")
     return False
-  
+
   return True
 
 # -------------------------------------------------------------------------------
@@ -323,41 +323,54 @@ def verify_register_write(register_name, exp_val, reg_query, reg_result):
 # according to the values specified in the current / default period
 #
 def inverter_update_routine():
-  read_config() # Reads the config according to the periods
+  read_config()  # Reads the config according to the periods
   inverter.connect()
   values = read_values()
   soe = values["batteries"]["Battery1"].get("soe")
+  battery_capacity = values["batteries"]["Battery1"].get("rated_energy")
+  battery_manufacturer = values["batteries"]["Battery1"].get("c_manufacturer")
+  if battery_manufacturer == "SolarEdge":
+    battery_capacity = round(battery_capacity / 0.9)
+  charing_limit_15p = round(battery_capacity * 0.15, -2)
   rc_cmd_mode = values["storage"].get("rc_cmd_mode")
   rc_charge_limit = values["storage"].get("rc_charge_limit")
   storage_backup_reserved_setting = values["storage"].get("storage_backup_reserved_setting")
-  
-  if (soe >= UPPER_CHARGING_LIMIT and rc_cmd_mode != 5):
-    LOGGER.info(f"SOE {round(soe, 2)}%. Reached upper limit of {UPPER_CHARGING_LIMIT}%.")
+
+  if soe >= UPPER_CHARGING_LIMIT and rc_cmd_mode != 5:
+    LOGGER.info(f"SoC {round(soe, 2)}%. Reached upper limit of {UPPER_CHARGING_LIMIT}%.")
     LOGGER.info("Setting \"rc_cmd_timeout\" to 8h.")
-    set_rc_cmd_timeout(28800) # 8 Hours
-    LOGGER.info("Setting \"set_rc_cmd_mode\" to \"5: Discahrge to match load\".")
+    set_rc_cmd_timeout(28800)  # 8 Hours
+    LOGGER.info("Setting \"set_rc_cmd_mode\" to \"5: Discharge to match load\".")
     set_rc_cmd_mode(5)
 
-  if (soe < (UPPER_CHARGING_LIMIT - SOE_DELTA_CHARGE) and rc_cmd_mode != 7):
-    LOGGER.info(f"SOE {round(soe, 2)}%. Dropped by delta of {SOE_DELTA_CHARGE}%.")
+  if soe < (UPPER_CHARGING_LIMIT - SOE_DELTA_CHARGE) and rc_cmd_mode != 7:
+    LOGGER.info(f"SoC {round(soe, 2)}%. Dropped by delta of {SOE_DELTA_CHARGE}%.")
     LOGGER.info("Setting \"rc_cmd_timeout\" to 1h.")
     set_rc_cmd_timeout()
     LOGGER.info("Setting \"set_rc_cmd_mode\" to \"7: Maximize self consumption\".")
     set_rc_cmd_mode(7)
 
-  if (rc_charge_limit != CHARGE_LIMIT):
+  # For the last 5%, reduce the charging power to 0.15C in order to increase stop charging accurancy
+  if (rc_charge_limit > charing_limit_15p and soe >= UPPER_CHARGING_LIMIT - 5):
+    LOGGER.info(f"Battery SoC is {round(soe, 2)}%. " +
+                f"Lowering charging power to {charing_limit_15p} W. (0.15C) in order to increase stop charging accurancy.")
+    LOGGER.info(f"Current battery charge limit: {rc_charge_limit} W.")
+    LOGGER.info(f"Setting battery charge limit to: {charing_limit_15p} W.")
+    set_rc_charge_limit(charing_limit_15p)
+
+  if rc_charge_limit != CHARGE_LIMIT and soe < UPPER_CHARGING_LIMIT - 5:
     LOGGER.info(f"Current battery charge limit: {rc_charge_limit} W.")
     LOGGER.info(f"Setting battery charge limit to: {CHARGE_LIMIT} W.")
     set_rc_charge_limit(CHARGE_LIMIT)
 
-  if (storage_backup_reserved_setting != BACKUP_RESEVE):
-    LOGGER.info(f"Current backup reserve: {rc_charge_limit}%.")
+  if storage_backup_reserved_setting != BACKUP_RESEVE:
+    LOGGER.info(f"Current backup reserve: {storage_backup_reserved_setting}%.")
     LOGGER.info(f"Setting backup reserve to: {BACKUP_RESEVE}%.")
-    set_storage_backup_reserved(BACKUP_RESEVE)    
-  
+    set_storage_backup_reserved(BACKUP_RESEVE)
+
   inverter.disconnect()
 
-# -------------------------------------------------------------------------------    
+# -------------------------------------------------------------------------------
 
 if __name__ == "__main__":
   argparser = argparse.ArgumentParser()
@@ -368,31 +381,31 @@ if __name__ == "__main__":
   argparser.add_argument("--info", action="store_true", default=False, help="Print all inverter settings")
 
   argparser.add_argument("--enable_storage_remote_control_mode", action="store_true", default=False,
-      help="Set the \"storage_contol_mode\" to \"4. Remote Control\". " +
-       "Neccessary for the storage profiles to be considered. It must be done once. " +
-       "Check the status with --info. Only after successful operation the script will work.")
-  
+                         help="Set the \"storage_contol_mode\" to \"4. Remote Control\". " +
+                              "Neccessary for the storage profiles to be considered. It must be done once. " +
+                              "Check the status with --info. Only after successful operation the script will work.")
+
   argparser.add_argument("--set_storage_default_mode", type=int, choices=[0, 1, 2, 3, 4, 5, 7], default=-1,
-      help="Set the default storage charge / discharge default mode (\"storage_default_mode\"). " + 
-      "Following options are available: 0. Off; 1. Charge from excess PV power only; " +
-      "2. Charge from PV first; 3. Charge from PV and AC; 4. Maximize export; " +
-      "5. Discharge to match load; 7. Maximize self consumption. " + 
-      "When using the --enable_storage_remote_control_mode to enable the remote control of the storage control, " +
-       "the \"storage_default_mode\" is set to \"7. Maximize self consumption\".") 
-  
+                         help="Set the default storage charge / discharge default mode (\"storage_default_mode\"). " +
+                              "Following options are available: 0. Off; 1. Charge from excess PV power only; " +
+                              "2. Charge from PV first; 3. Charge from PV and AC; 4. Maximize export; " +
+                              "5. Discharge to match load; 7. Maximize self consumption. " +
+                              "When using the --enable_storage_remote_control_mode to enable the remote control of the storage control, " +
+                              "the \"storage_default_mode\" is set to \"7. Maximize self consumption\".")
+
   args = argparser.parse_args()
 
   # Setup logging to console & file
   LOGGER = logging.getLogger("se_battery_control")
   LOGGER.setLevel(LOGGER_LEVEL)
   log_formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(filename)s::%(lineno)d: %(message)s")
-  
+
   console = logging.StreamHandler()
   console.setLevel(LOGGER_LEVEL)
   console.setFormatter(log_formatter)
   LOGGER.addHandler(console)
-  rotationLogHandler = RotatingFileHandler(LOG_FILE, mode='a', maxBytes=5*1024*1024, 
-                                   backupCount=20, encoding='utf-8', delay=0)
+  rotationLogHandler = RotatingFileHandler(LOG_FILE, mode='a', maxBytes=5 * 1024 * 1024,
+                                           backupCount=20, encoding='utf-8', delay=0)
   rotationLogHandler.setFormatter(log_formatter)
   rotationLogHandler.setLevel(LOGGER_LEVEL)
   LOGGER.addHandler(rotationLogHandler)
@@ -408,7 +421,7 @@ if __name__ == "__main__":
   storage = solaredge_modbus.StorageInverter(parent=inverter)
 
   if args.info:
-    values = read_values() 
+    values = read_values()
     LOGGER.info(json.dumps(values, indent=2))
     exit()
 
@@ -428,7 +441,7 @@ if __name__ == "__main__":
   # In order to be used as CronJob - just runs once
   inverter_update_routine()
 
-  # Alternatevly, an infinite loop can be used instead of CronJob - runs every UPDATE_INTERVAL
+  # Alternately, an infinite loop can be used instead of CronJob - runs every UPDATE_INTERVAL
   # Installing it as a service in this case is recommended in order to have automatic restarts
   # Before the "solaredge_modbus" library is fixed to work good with the storage registers, better use CronJob
   #
@@ -436,4 +449,4 @@ if __name__ == "__main__":
   #   inverter_update_routine()
   #   time.sleep(UPDATE_INTERVAL)
 
-  # -------------------------------------------------------------------------------    
+  # -------------------------------------------------------------------------------
